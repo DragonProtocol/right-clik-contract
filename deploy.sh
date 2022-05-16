bde31572fd5478f0bdb80a00c0a21659b34930ba
lmf004 <liumf2005@126.com>
2022-05-16 18:19:03 +0800

    modify deploy.sh for RightClik

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 echo "eth rinkeby:"
-forge create --rpc-url https://rinkeby.infura.io/v3/aaf906eab31742d2879c3d0a857dbeb9 --constructor-args "RightClik DAO NFT" "RCNFT" --private-key d042cab62c4a28cb3aca6dbd3edcebbb160ba4cd7a8226034824ddeaa186645e src/SyntheticNFT.sol:SyntheticNFT
+forge create --rpc-url https://rinkeby.infura.io/v3/aaf906eab31742d2879c3d0a857dbeb9 --constructor-args "RightClik DAO NFT" "RCNFT" --private-key d042cab62c4a28cb3aca6dbd3edcebbb160ba4cd7a8226034824ddeaa186645e src/RightClik.sol:RightClik
 
